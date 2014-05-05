36fb1fc9b3942c578e58d14c32651b0a7e58e3d0
Eko S. Wibowo <swdev.bali@gmail.com>
2014-05-05 12:55:30 +0700

    leaner init.el

---
+++ init.el
@@ -0,0 +1,15 @@
+;maximize screen space
+(scroll-bar-mode 0)
+(menu-bar-mode 0)
+(tool-bar-mode 0)
+
+;easier back<->forward navigation using C-C LEFT, C-C RIGHT
+(winner-mode 1)
+
+;my personal preference for coding in emacs
+(set-background-color "Black")
+(set-foreground-color "White")
+
+;make CTRL+C, CTRL+V as copy & paste shortcut as in Windows
+(cua-mode 1)
+
