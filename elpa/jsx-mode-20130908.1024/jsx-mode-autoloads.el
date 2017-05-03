6bb5d6b670387dde9211082cbf3e6eb353a9af27
Eko Wibowo <eko@monetizemore.com>
2017-05-03 12:53:09 +0700

    elpa

---
+++ elpa/jsx-mode-20130908.1024/jsx-mode-autoloads.el
@@ -0,0 +1,22 @@
+;;; jsx-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "jsx-mode" "jsx-mode.el" (22784 51449 786559
+;;;;;;  363000))
+;;; Generated autoloads from jsx-mode.el
+
+(autoload 'jsx-mode "jsx-mode" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; jsx-mode-autoloads.el ends here
